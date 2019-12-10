a51b1987f5738bd01d09b938970d9c10d6254dfd
paqits <monaf2@illinois.edu>
2019-12-10 10:02:25 -0600

    XML changes

--- app/src/main/java/com/example/yogamaster/MainActivity.java
+++ app/src/main/java/com/example/yogamaster/MainActivity.java
@@ -3,19 +3,107 @@ package com.example.yogamaster;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
+    //private Button button;
+    private TextView countdownText;
+    private Button countdownButton;
+    private Button resetButton;
+    private CountDownTimer countDownTimer;
+    private long timeLeft = 30000;
+    private boolean timerRunning;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        countdownText = findViewById(R.id.countdown_text);
+        countdownButton = findViewById(R.id.countdown_button);
+        resetButton = findViewById(R.id.button);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
 
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
         viewPager.setAdapter(viewPagerAdapter);
 
+//        final ArrayList<Integer> selectedImages = viewPagerAdapter.selectedImages;
+//        button = (Button) findViewById(R.id.button);
+        countdownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+//                Bundle input = new Bundle();
+//                for (int i = 0; i < selectedImages.size(); i++) {
+//                    input.putInt("" + i, selectedImages.get(i));
+//                }
+//                intent.putExtras(input);
+//                startActivity(intent);
+                  startStop();
+            }
+        });
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+//                Bundle input = new Bundle();
+//                for (int i = 0; i < selectedImages.size(); i++) {
+//                    input.putInt("" + i, selectedImages.get(i));
+//                }
+//                intent.putExtras(input);
+//                startActivity(intent);
+                timeLeft = 30000;
+                updateTimer();
+            }
+        });
+
+
+    }
+    public void startStop() {
+        if (timerRunning) {
+            stopTimer();
+        } else {
+            startTimer();
+        }
+    }
+
+    public void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeft, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeft = l;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+        countdownButton.setText("PAUSE");
+        timerRunning = true;
+    }
+    public void stopTimer() {
+        countDownTimer.cancel();
+        countdownButton.setText("START");
+        timerRunning = false;
+    }
+    public void updateTimer() {
+        int minutes = (int) timeLeft / 60000;
+        int seconds = (int) timeLeft % 60000 / 1000;
+
+        String timeLeftText;
+
+        timeLeftText = "" +minutes;
+        timeLeftText += ":";
+        if (seconds < 10) timeLeftText += "0";
+        timeLeftText += seconds;
+        countdownText.setText(timeLeftText);
     }
 }
--- app/src/main/java/com/example/yogamaster/ViewPagerAdapter.java
+++ app/src/main/java/com/example/yogamaster/ViewPagerAdapter.java
@@ -5,7 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
@@ -15,8 +17,8 @@ public class ViewPagerAdapter extends PagerAdapter {
 
     private Context context;
     private LayoutInflater layoutInflater;
-    private Integer[] images = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3};
-
+    private Integer[] images = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3, R.drawable.slide4, R.drawable.slide5, R.drawable.slide6, R.drawable.slide7, R.drawable.slide8, R.drawable.slide9, R.drawable.slide10, R.drawable.slide11};
+    public ArrayList<Integer> selectedImages = new ArrayList<Integer>();
     public ViewPagerAdapter(Context context) {
         this.context = context;
     }
@@ -36,8 +38,19 @@ public class ViewPagerAdapter extends PagerAdapter {
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = layoutInflater.inflate(R.layout.custom_layout, null);
-        ImageView imageView= (ImageView) view.findViewById(R.id.imageView);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
         imageView.setImageResource(images[position]);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < images.length; i++) {
+                    selectedImages.add(images[i]);
+                    Toast.makeText(context, "selected", Toast.LENGTH_SHORT).show();
+                    //action for when slide is clicked
+                }
+            }
+        });
+
         ViewPager vp = (ViewPager) container;
         vp.addView(view, 0);
         return view;
@@ -49,4 +62,5 @@ public class ViewPagerAdapter extends PagerAdapter {
         View view =(View) object;
         vp.removeView(view);
     }
+
 }
